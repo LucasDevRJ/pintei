d85d9fd327acad76187e82396ab589ce14e07682
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-01 18:02:08 -0300

    Pegando o x e y do clique

--- javaScript/canvas.js
+++ javaScript/canvas.js
@@ -4,3 +4,11 @@ var pincel = tela.getContext("2d");
 pincel.fillStyle = "lightgray";
 pincel.fillRect(0, 0, 1350, 625);
 pincel.fill();
+
+function clicouNoCanvas(evento) {
+	var x = evento.pageX;
+	var y = evento.pageY;
+	alert("Posição do clique: " + x + ", " + y);
+}
+
+tela.onclick = clicouNoCanvas;
