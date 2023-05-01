51d752344b5fd93347bfc26835de13170e816e98
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-01 18:16:56 -0300

    Função para pintar circulos no canvas

--- javaScript/canvas.js
+++ javaScript/canvas.js
@@ -6,9 +6,13 @@ pincel.fillRect(0, 0, 1350, 625);
 pincel.fill();
 
 function clicouNoCanvas(evento) {
-	var x = evento.pageX;
-	var y = evento.pageY;
-	alert("Posição do clique: " + x + ", " + y);
+	var x = evento.pageX - tela.offsetLeft;
+	var y = evento.pageY - tela.offsetTop;
+	
+	pincel.fillStyle = "blue";
+	pincel.beginPath();
+	pincel.arc(x, y, 10, 0, 2*3.14);
+	pincel.fill();
 }
 
 tela.onclick = clicouNoCanvas;
